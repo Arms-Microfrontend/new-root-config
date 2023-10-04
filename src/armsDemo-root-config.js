c33ce9365e5fa95ee52ddf4fc468f53e86f278a3
amalisaah <100369935+amalisaah@users.noreply.github.com>
2023-10-04 14:31:44 +0000

    imported first application

--- src/armsDemo-root-config.js
+++ src/armsDemo-root-config.js
@@ -1,19 +1,19 @@
 import { registerApplication, start } from "single-spa";
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
-});
-
 // registerApplication({
-//   name: "@armsDemo/navbar",
-//   app: () => System.import("@armsDemo/navbar"),
-//   activeWhen: ["/"]
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
 // });
+
+registerApplication({
+  name: "@armsDemo/homepage",
+  app: () => System.import("@armsDemo/homepage"),
+  activeWhen: ["/home"]
+});
 
 start({
   urlRerouteOnly: true,
